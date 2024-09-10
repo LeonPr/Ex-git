dcefb8ac73d0f200f23e5f75e441a61f1df0d8f1
Leon Pri-Mor <leon@masig.co.il>
2024-09-10 15:47:05 +0300

    Limit ball size

--- js/main.js
+++ js/main.js
@@ -1,9 +1,19 @@
 
-function onBallClick() {
+const gMaxSize = 400
+const gMinSize = 100
+var gCurrentSize = 100
 
+function onBallClick() {
+    var ballGrow=''
     const elBall = document.querySelector('.ball')
-    console.log('elBall.style.height', elBall.style.height);
-    ///elBall.style.height = elBall.style.height + 50px
-    elBall.style.width = '150%'
-    elBall.innerText = '150'
+    if (gCurrentSize >= gMinSize && gCurrentSize < gMaxSize) {
+        ballGrow=`${gCurrentSize += 50}px`
+    
+    }    else{
+        ballGrow=`${gMinSize}px`
+        gCurrentSize=100
+    }
+    elBall.style.width = ballGrow
+    elBall.style.height = ballGrow
+    elBall.innerText = gCurrentSize
 }
