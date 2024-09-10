87bdce2c910d77929bd1498c8bc00aa4f03e934e
LeonPr <leon@masig.co.il>
2024-09-10 22:01:38 +0300

    Add new ball

--- js/main.js
+++ js/main.js
@@ -3,10 +3,10 @@ const gMaxSize = 400
 const gMinSize = 100
 var gCurrentSize = 100
 
-function onBallClick() {
+function onBallClick(elBall,maxDiameter) {
     var ballGrow=''
-    const elBall = document.querySelector('.ball')
-    if (gCurrentSize >= gMinSize && gCurrentSize < gMaxSize) {
+    //const elBall = document.querySelector('.ball')
+    if (gCurrentSize >= gMinSize && gCurrentSize < maxDiameter) {
         ballGrow=`${gCurrentSize += getRandomInt(20,60)}px`
     }    else{
         ballGrow=`${gMinSize}px`
